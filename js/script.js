72db07bb9590c59623640fb40be633ece5bdee6b
Marcin <mbielen1995@gmail.com>
2021-01-15 15:45:35 +0100

    Add relational operators

--- js/script.js
+++ js/script.js
@@ -12,6 +12,15 @@ var name = 'Marcin' ;
 //sum
 alert(x + y) ;
 
+y = '5' ;
+alert(x == y) ;
+alert(x === y) ;
+y = 6;
+alert(x != y) ;
+alert(x !== y);
+
+// < > <= >=
+
 alert(name) ;
 
 alert(x + y + ' ' + name) ;
