21f5993c917d3d537b4c1d67361bbc2c66bc0468
Marcin <mbielen1995@gmail.com>
2021-01-15 16:04:15 +0100

    Add conditional

--- js/script.js
+++ js/script.js
@@ -9,18 +9,27 @@ var x = 5 ;
 var y = 6 ;
 var name = 'Marcin' ;
 
+if (x < y)
+  alert("x is smaller than y");
+else if (x > y)
+  alert("x is greater than y");
+else {
+  alert("x is equal to y");
+  alert("text")
+}
+
 //sum
-alert(x + y) ;
+//alert(x + y) ;
 
 y = '5' ;
-alert(x == y) ;
-alert(x === y) ;
+//alert(x == y) ;
+//alert(x === y) ;
 y = 6;
-alert(x != y) ;
-alert(x !== y);
+//alert(x != y) ;
+//alert(x !== y);
 
 // < > <= >=
 
-alert(name) ;
+//alert(name) ;
 
-alert(x + y + ' ' + name) ;
+//alert(x + y + ' ' + name) ;
