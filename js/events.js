fbd62165edf030001b813e6e32ee1e5129b503cc
Marcin <mbielen1995@gmail.com>
2021-01-16 17:50:01 +0100

    Add onmouseover, onmouseout on event element

--- js/events.js
+++ js/events.js
@@ -4,7 +4,15 @@ function writeText(text) {
 function writeClicked() {
   alert("Clicked!");
 }
+function changeColor() {
+  this.className = "myColor";
+}
+function changeColor2() {
+  this.removeAttribute("class");
+}
 
 var testEvent = document.getElementById("testEvent");
 
+testEvent.onmouseover = changeColor;
+testEvent.onmouseout = changeColor2;
 testEvent.onclick = writeClicked;
