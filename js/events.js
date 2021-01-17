1f76bb63e8b6379d15923fece57cb3619b096c7c
Marcin <mbielen1995@gmail.com>
2021-01-17 15:05:01 +0100

    Add event listeners

--- js/events.js
+++ js/events.js
@@ -1,3 +1,12 @@
+//this function enable to eventlisteners work on more browsers
+function createEvent(obj, eventName, functionToInvoke) {
+  if (document.addEventListener)
+    obj.addEventListener(eventName, functionToInvoke);
+  else
+    obj.attachEvent("on" + eventName, functionToInvoke);
+}
+
+
 function writeText(text) {
   alert(text + "!");
 }
@@ -10,12 +19,30 @@ function changeColor() {
 function changeColor2() {
   this.removeAttribute("class");
 }
+function increaseFont() {
+  var fontSize = parseInt(window.getComputedStyle(this).fontSize);
+  this.style.fontSize = (++fontSize) + "px";
+}
 
 //code is executed after site is loaded, async in script source link in html is not necessary
 window.onload = function () {
   var testEvent = document.getElementById("testEvent");
-
+  var stopEvent = document.getElementById("stopIncreasingFont");
+  /*
   testEvent.onmouseover = changeColor;
   testEvent.onmouseout = changeColor2;
   testEvent.onclick = writeClicked;
+  */
+  /*
+  testEvent.addEventListener("mouseover", changeColor);
+  testEvent.addEventListener("mouseover", increaseFont);
+  testEvent.addEventListener("mouseout", changeColor2);
+  */
+  createEvent(testEvent, "mouseover", changeColor);
+  createEvent(testEvent, "mouseover", increaseFont);
+  createEvent(testEvent, "mouseout", changeColor2);
+
+  stopEvent.addEventListener("click", function () {
+    testEvent.removeEventListener("mouseover", increaseFont)
+  })
 };
